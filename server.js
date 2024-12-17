3d427919a85a231cd1126c330a1461900e69d703
Amit Kulkarni <amit.kulkarni@sunbeaminfo.com>
2024-12-17 13:20:40 +0530

    second commit

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const express = require('express')
 const app = express()
 
 app.get('/', (request, response) => {
-  response.send('version 1.0')
+  response.send('version 1.1')
 })
 
 app.listen(4000, '0.0.0.0', () => {
